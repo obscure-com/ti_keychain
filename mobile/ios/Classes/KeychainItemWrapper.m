58db8dbbb1190126e714f859b806267a3264a069
Paul Mietz Egli <paul@obscure.com>
2012-05-24 10:41:36 -0700

    fixed issue #2

--- mobile/ios/Classes/KeychainItemWrapper.m
+++ mobile/ios/Classes/KeychainItemWrapper.m
@@ -101,7 +101,7 @@ Keychain API expects as a validly constructed container class.
         genericPasswordQuery = [[NSMutableDictionary alloc] init];
         
 		[genericPasswordQuery setObject:(id)kSecClassGenericPassword forKey:(id)kSecClass];
-        [genericPasswordQuery setObject:identifier forKey:(id)kSecAttrGeneric];
+        [genericPasswordQuery setObject:identifier forKey:(id)kSecAttrAccount];
 		
 		// The keychain access group attribute determines if this item can be shared
 		// amongst multiple apps whose code signing entitlements contain the same keychain access group.
@@ -135,7 +135,7 @@ Keychain API expects as a validly constructed container class.
             [self resetKeychainItem];
 			
 			// Add the generic attribute and the keychain access group.
-			[keychainItemData setObject:identifier forKey:(id)kSecAttrGeneric];
+			[keychainItemData setObject:identifier forKey:(id)kSecAttrAccount];
 			if (accessGroup != nil)
 			{
 #if TARGET_IPHONE_SIMULATOR
