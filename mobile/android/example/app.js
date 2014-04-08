64d4dd5a36c02bc7d03dc88c8f2d8f40f8642671
Paul Mietz Egli <paul@obscure.com>
2014-04-08 14:41:39 -0700

    updated the Android version to match the API of the iOS version; added ti-mocha unit tests.

--- mobile/android/example/app.js
+++ mobile/android/example/app.js
@@ -1,77 +1,17 @@
-// This is a test harness for your module
-// You should do something interesting in this harness 
-// to test out the module and to provide instructions 
-// to users on how to use it by example.
+require('ti-mocha');
 
+// import tests
+require('001_module')();
+require('002_keychainitem')();
 
-// open a single window
+// create a window and run the tests
 var window = Ti.UI.createWindow({
-	backgroundColor:'white',
-	layout: 'vertical'
+  layout: 'vertical',
+  backgroundColor: 'white'
 });
 
-var label = Ti.UI.createLabel({
-  top: 20,
-  height: 30,
-  left: 10,
-  right: 10,
-  text: 'keychain test',
-});
-window.add(label);
-
-var button1 = Ti.UI.createButton({
-  title: 'set account',
-  top: 10,
-  height: 30,
-  width: 200,
-});
-button1.addEventListener('click', function() {
-  keychainItem.account = 'pegli';
-  label.text = "new account = "+keychainItem.account;
-});
-window.add(button1);
-
-var button2 = Ti.UI.createButton({
-  title: 'set password',
-  top: 10,
-  height: 30,
-  width: 200,
-});
-button2.addEventListener('click', function() {
-  keychainItem.valueData = 'supersecret';
-  label.text = "new password = "+keychainItem.valueData;
-});
-window.add(button2);
-
-var button3 = Ti.UI.createButton({
-  title: 'reset keychain item',
-  top: 10,
-  height: 30,
-  width: 200,
-});
-button3.addEventListener('click', function() {
-  keychainItem.reset();
-  label.text = "reset item, account = "+keychainItem.account;
-});
-window.add(button3);
-
-var button4 = Ti.UI.createButton({
-  title: 'get keychain item',
-  top: 10,
-  height: 30,
-  width: 200,
-});
-button4.addEventListener('click', function() {
-  label.text = "account = " + keychainItem.account + "; password = " + keychainItem.valueData;
-});
-window.add(button4);
-
+window.addEventListener('open', function() {
+  mocha.run();
+})
 
 window.open();
-
-// TODO: write your module tests here
-var keychain = require('com.obscure.keychain');
-Ti.API.info("module is => " + keychain);
-
-var keychainItem = keychain.createKeychainItem('mylogin', 'supersecretpassphrase');
-Ti.API.info("account = " + keychainItem.account + "; password = " + keychainItem.valueData);
--- mobile/android/src/com/obscure/keychain/KeychainItemProxy.java
+++ mobile/android/src/com/obscure/keychain/KeychainItemProxy.java
@@ -13,100 +13,128 @@ import android.util.Log;
 @Kroll.proxy(creatableInModule = KeychainModule.class)
 public class KeychainItemProxy extends KrollProxy {
 
-	private static final String	ACCOUNT_KEY		= "keychain.account";
+    private static final String ACCOUNT_KEY     = "keychain.account";
 
-	private static final String	LCAT			= "KeychainItemProxy";
+    private static final String COMMENT_KEY     = "keychain.comment";
 
-	private static final String	VALUE_DATA_KEY	= "keychain.valueData";
+    private static final String DESCRIPTION_KEY = "keychain.description";
 
-	private ContextWrapper		androidContext;
+    private static final String LCAT            = "KeychainItemProxy";
 
-	private DesEncryptor		encryptor;
+    private static final String VALUE_DATA_KEY  = "keychain.valueData";
 
-	private SharedPreferences	sharedPrefs;
+    private ContextWrapper      androidContext;
 
-	// Constructor
-	public KeychainItemProxy(TiContext tiContext) {
-		super(tiContext);
-		androidContext = tiContext.getAndroidContext();
-	}
+    private DesEncryptor        encryptor;
 
-	private String fetchValueForKey(String key) {
-		String value = sharedPrefs.getString(key, null);
-		if (encryptor != null && value != null) {
-			try {
-				value = encryptor.decrypt(value);
-			}
-			catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		return value;
-	}
+    private String              identifier;
 
-	@Kroll.getProperty
-	@Kroll.method
-	public String getAccount() {
-		return fetchValueForKey(ACCOUNT_KEY);
-	}
+    private SharedPreferences   sharedPrefs;
 
-	@Kroll.getProperty
-	@Kroll.method
-	public String getValueData() {
-		return fetchValueForKey(VALUE_DATA_KEY);
-	}
+    // Constructor
+    public KeychainItemProxy(TiContext tiContext) {
+        super(tiContext);
+        androidContext = tiContext.getAndroidContext();
+    }
 
-	@Override
-	public void handleCreationArgs(KrollModule createdInModule, Object[] args) {
-		super.handleCreationArgs(createdInModule, args);
-		if (args != null) {
+    private String fetchValueForKey(String key) {
+        String value = sharedPrefs.getString(key, null);
+        if (encryptor != null && value != null) {
+            try {
+                value = encryptor.decrypt(value);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return value != null ? value : "";
+    }
 
-			if (args.length > 0) {
-				String name = String.format("keychain.%s", (String) args[0]);
-				sharedPrefs = androidContext.getSharedPreferences(name, 0);
-			}
-			if (args.length > 1) {
-				try {
-					encryptor = new DesEncryptor((String) args[1]);
-				}
-				catch (Exception e) {
-					Log.d(LCAT, "error creating keystore", e);
-				}
-			}
-		}
-	}
+    @Kroll.getProperty(name = "account")
+    public String getAccount() {
+        return fetchValueForKey(ACCOUNT_KEY);
+    }
 
-	@Kroll.method
-	public void reset() {
-		SharedPreferences.Editor editor = sharedPrefs.edit();
-		editor.remove(ACCOUNT_KEY);
-		editor.remove(VALUE_DATA_KEY);
-		editor.commit();
-	}
+    @Kroll.getProperty(name = "comment")
+    public String getComment() {
+        return fetchValueForKey(COMMENT_KEY);
+    }
 
-	@Kroll.setProperty
-	@Kroll.method
-	public void setAccount(String value) {
-		storeValueForKey(ACCOUNT_KEY, value);
-	}
+    @Kroll.getProperty(name = "description")
+    public String getDescription() {
+        return fetchValueForKey(DESCRIPTION_KEY);
+    }
 
-	@Kroll.setProperty
-	@Kroll.method
-	public void setValueData(String value) {
-		storeValueForKey(VALUE_DATA_KEY, value);
-	}
+    @Kroll.getProperty(name = "identifier")
+    public String getIdentifier() {
+        return identifier;
+    }
 
-	private void storeValueForKey(String key, String value) {
-		if (encryptor != null) {
-			try {
-				value = encryptor.encrypt(value);
-			}
-			catch (Exception e) {
-				Log.d(LCAT, "", e);
-			}
-		}
-		SharedPreferences.Editor editor = sharedPrefs.edit();
-		editor.putString(key, value);
-		editor.commit();
-	}
+    @Kroll.getProperty(name = "valueData")
+    public String getValueData() {
+        return fetchValueForKey(VALUE_DATA_KEY);
+    }
+
+    @Override
+    public void handleCreationArgs(KrollModule createdInModule, Object[] args) {
+        super.handleCreationArgs(createdInModule, args);
+        if (args != null) {
+
+            if (args.length > 0) {
+                this.identifier = (String) args[0];
+                String name = String.format("keychain.%s", this.identifier);
+                sharedPrefs = androidContext.getSharedPreferences(name, 0);
+            }
+            if (args.length > 1) {
+                try {
+                    encryptor = new DesEncryptor((String) args[1]);
+                }
+                catch (Exception e) {
+                    Log.d(LCAT, "error creating keystore", e);
+                }
+            }
+        }
+    }
+
+    @Kroll.method
+    public void reset() {
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.remove(ACCOUNT_KEY);
+        editor.remove(VALUE_DATA_KEY);
+        editor.commit();
+    }
+
+    @Kroll.setProperty(name = "account")
+    public void setAccount(String value) {
+        storeValueForKey(ACCOUNT_KEY, value);
+    }
+
+    @Kroll.setProperty(name = "comment")
+    public void setComment(String value) {
+        storeValueForKey(COMMENT_KEY, value);
+    }
+
+    @Kroll.setProperty(name = "description")
+    public void setDescription(String value) {
+        storeValueForKey(DESCRIPTION_KEY, value);
+    }
+
+    @Kroll.setProperty(name = "valueData")
+    public void setValueData(String value) {
+        storeValueForKey(VALUE_DATA_KEY, value);
+    }
+
+    private void storeValueForKey(String key, String value) {
+        if (encryptor != null) {
+            try {
+                value = encryptor.encrypt(value);
+            }
+            catch (Exception e) {
+                Log.d(LCAT, "", e);
+            }
+        }
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putString(key, value);
+        editor.commit();
+    }
 }
